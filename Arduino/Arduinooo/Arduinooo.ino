2e359fa4b2debafb5ac0c40a6629030702656306
Allen Román <2909080001301@ingenieria.usac.edu.gt>
2022-12-08 22:25:44 -0600

    Código Java

--- Arduino/Arduinooo/Arduinooo.ino
+++ Arduino/Arduinooo/Arduinooo.ino
@@ -22,6 +22,8 @@ int columnas[] = {22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37
 int filas[] = {38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 2, 4, 7, 8, 10, 11, 12, 13};
 
 void setup() {
+
+  Serial.begin(9600);
   // put your setup code here, to run once:
   for (int i = 0; i < 24; i++) {
     pinMode(filas[i], OUTPUT);
@@ -34,6 +36,13 @@ void setup() {
 }
 
 void loop() {
+
+  if(Serial.available()>0){
+    String imprPantalla = Serial.readString();
+    Serial.println(imprPantalla);
+    }
+
+    
   int c = 0;
   for (int col = 0; col < 16; col++) {
 
---
+++ Java/src/ArduinoPr1/Arduino.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ArduinoPr1;
+
+//https://fazecast.github.io/jSerialComm/
+import com.fazecast.jSerialComm.SerialPort;
+import java.io.IOException;
+
+/**
+ *
+ * @author "el profe Ariel"
+ */
+public class Arduino {
+    private SerialPort puertoSerie;      
+    
+    public boolean conectar(int indice){        
+        puertoSerie = SerialPort.getCommPorts()[indice];
+        puertoSerie.openPort();
+        puertoSerie.setComPortParameters(9600, 8, 1, 0);
+        puertoSerie.setComPortTimeouts(SerialPort.TIMEOUT_WRITE_BLOCKING, 0, 0); 
+        return estaConectado();
+    }
+    
+    public static SerialPort[] getPuertosSerie() {
+        SerialPort puertos[] = SerialPort.getCommPorts();
+        return puertos;
+    }    
+    
+
+    public boolean estaConectado() {
+        return puertoSerie.isOpen();
+    }
+
+    public boolean desconectar() {
+        return puertoSerie.closePort();
+    }
+
+    
+    public boolean enviarLetra(char valor) {
+        try {
+            puertoSerie.getOutputStream().write(valor);
+            puertoSerie.getOutputStream().flush();
+            return true;
+        } catch (IOException ex) {           
+            return false;
+        }        
+    }
+    
+    public boolean enviarTexto(String texto) {
+        try {
+            puertoSerie.getOutputStream().write(texto.getBytes());
+            puertoSerie.getOutputStream().flush();
+            return true;
+        } catch (IOException ex) {            
+            return false;
+        }        
+    }
+
+}
